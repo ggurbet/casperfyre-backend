2a21f35b7fe72be0bb312f20f29e8f1b115bef46
= <=>
2022-04-05 19:26:50 -0400

    2fa adjust

--- public/user_api/submit-mfa.php
+++ public/user_api/submit-mfa.php
@@ -9,51 +9,66 @@ $mfa_code = $params['mfa_code'] ?? null;
 $guid = $params['guid'] ?? null;
 
 $query = "
-	SELECT code
+	SELECT code, created_at
 	FROM twofa
 	WHERE guid = '$guid'
 	AND code = '$mfa_code'
 ";
 $selection = $db->do_select($query);
-$selection = $selection[0]['code'];
+$fetched_code = $selection[0]['code'] ?? '';
+$created_at = $selection[0]['created_at'] ?? 0;
+$expire_time = $helper->get_datetime(-600); // 10 minutes ago
 
-if($mfa_code == $selection) {
-	$query = "
-		DELETE FROM twofa
-		WHERE guid = '$guid'
-	";
-	$db->do_query($query);
+if($selection && $mfa_code == $fetched_code) {
+	if($expire_time < $created_at) {
+		$query = "
+			DELETE FROM twofa
+			WHERE guid = '$guid'
+		";
+		$db->do_query($query);
 
-	$bearer = $helper->generate_session_token();
-	$created_at = $helper->get_datetime();
-	$expires_at = $helper->get_datetime(86400); // one day from now
+		$bearer = $helper->generate_session_token();
+		$created_at = $helper->get_datetime();
+		$expires_at = $helper->get_datetime(86400); // one day from now
 
-	$query1 = "
-		DELETE FROM sessions
-		WHERE guid = '$guid'
-	";
+		$query1 = "
+			DELETE FROM sessions
+			WHERE guid = '$guid'
+		";
 
-	$query2 = "
-		INSERT INTO sessions (
-		guid,
-		bearer,
-		created_at,
-		expires_at
-		) VALUES (
-		'$guid',
-		'$bearer',
-		'$created_at',
-		'$expires_at'
-		)
-	";
+		$query2 = "
+			INSERT INTO sessions (
+			guid,
+			bearer,
+			created_at,
+			expires_at
+			) VALUES (
+			'$guid',
+			'$bearer',
+			'$created_at',
+			'$expires_at'
+			)
+		";
 
-	$db->do_query($query1);
-	$db->do_query($query2);
+		$db->do_query($query1);
+		$db->do_query($query2);
 
-	_exit(
-		'success',
-		$bearer
-	);
+		_exit(
+			'success',
+			$bearer
+		);
+	} else {
+		$query = "
+			DELETE FROM twofa
+			WHERE guid = '$guid'
+		";
+		$db->do_query($query);
+
+		_exit(
+			'error',
+			'MFA code expired. Please try logging back in'
+		);
+	}
 }
 
 _exit(
