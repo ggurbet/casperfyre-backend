3278acb66b022fe4fd4a006ba0d474018b3d697c
= <=>
2022-04-25 13:29:28 -0400

    admin apy key handling

--- public/admin_api/enable-apikey.php
+++ public/admin_api/enable-apikey.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *
- * POST /admin/disable-apikey
+ * POST /admin/enable-apikey
  *
  * HEADER Authorization: Bearer
  *
@@ -19,7 +19,7 @@ $api_key_id = (int)($params['api_key_id'] ?? 0);
 
 $query = "
 	UPDATE api_keys
-	SET active = 0
+	SET active = 1
 	WHERE id = $api_key_id
 ":
 $result = $db->do_query($query);
@@ -27,13 +27,13 @@ $result = $db->do_query($query);
 if($result) {
 	_exit(
 		'success',
-		'Successfully disabled API key'
+		'Successfully enabled API key'
 	);
 }
 
 _exit(
 	'error',
-	'Failed to disable api key',
+	'Failed to enable api key',
 	500,
-	'Failed to disable api key'
+	'Failed to enable api key'
 );
--- public/admin_api/history.php
+++ public/admin_api/history.php
@@ -16,11 +16,19 @@ $auth = authenticate_session(2);
 $admin_guid = $auth['guid'] ?? '';
 $user_guid = _request('guid');
 
-$query = "
-	SELECT * 
-	FROM orders
-	WHERE guid = '$user_guid'
-";
+if($user_guid && $user_guid != '') {
+	$query = "
+		SELECT * 
+		FROM orders
+		WHERE guid = '$user_guid'
+	";
+} else {
+	$query = "
+		SELECT * 
+		FROM orders
+	";
+}
+
 $results = $db->do_select($query);
 
 _exit(
