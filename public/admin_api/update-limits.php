77038705eb82b4f7dd6a437bde5165a205490b30
= <=>
2022-04-25 13:50:12 -0400

    update limits check guid

--- public/admin_api/update-limits.php
+++ public/admin_api/update-limits.php
@@ -9,6 +9,8 @@
  * @param int     per_limit
  * @param int     day_limit
  * @param int     month_limit
+ *
+ * If a parameter is not specified, then it will not be affected.
  */
 include_once('../../core.php');
 
@@ -23,6 +25,22 @@ $per_limit = $params['per_limit'] ? (int)$params['per_limit'] : null;
 $day_limit = $params['day_limit'] ? (int)$params['day_limit'] : null;
 $month_limit = $params['month_limit'] ? (int)$params['month_limit'] : null;
 $message = '';
+
+$query = "
+	SELECT id
+	FROM users
+	WHERE guid = '$user_guid'
+";
+$check = $db->do_select($query);
+
+if(!$check) {
+	_exit(
+		'error',
+		'User does not exist',
+		400,
+		'User does not exist'
+	);
+}
 
 if(gettype($per_limit) == 'integer') {
 	$query = "
