582deac36baf11c943603fb3ebb529ba56fd88b5
= <=>
2022-05-12 01:02:45 -0400

    order deployment pem

--- public/cron_jobs/orders.php
+++ public/cron_jobs/orders.php
@@ -19,7 +19,7 @@ $query = "
 	FROM orders
 	WHERE fulfilled = 0
 	AND return_code = 200
-	LIMIT 3
+	LIMIT 5
 ";
 
 $selection = $db->do_select($query);
@@ -31,6 +31,8 @@ if($selection) {
 		$wallet_id = (int)$s['wallet_id_used'];
 		$amount = (int)($s['amount'] ?? 0);
 		$sent_at = $helper->get_datetime();
+		$ready_to_deploy = true;
+		$stdout = '{}';
 
 		if(
 			ctype_xdigit($address) &&
@@ -38,7 +40,30 @@ if($selection) {
 		) {
 			/* generate pem from user's secret key hex */
 			$secret_key_hex = $helper->get_wallet_secret_key($wallet_id);
-			$secret_key_path = '';
+			$secret_key_path = (
+				BASE_DIR.'/tmp/sk-'.
+				(string)$wallet_id.'-'.
+				(string)$order_id.'-'.
+				(string)time().'.pem'
+			);
+
+			try {
+				$b64_key = base64_encode(hex2bin(
+					'302e020100300506032b657004220420'.$secret_key_hex
+				));
+
+				file_put_contents(
+					$secret_key_path,
+					(
+						"-----BEGIN PRIVATE KEY-----\n".
+						$b64_key.
+						"\n-----END PRIVATE KEY-----\n"
+					)
+				);
+			} catch(Exception $e) {
+				$ready_to_deploy = false;
+				elog('Failed to translate secret key hex to pem format for order #'.$order_id);
+			}
 
 			/* send deploy */
 			$command = "casper-client transfer";
@@ -49,13 +74,18 @@ if($selection) {
 			$command .= " --target-account ".$address;
 			$command .= " --payment-amount 100000000";
 			$command .= " --chain-name casper";
-			$stdout = shell_exec($command);
+
+			if($ready_to_deploy) {
+				$stdout = shell_exec($command);
+			}
+
 			$success = '';
 
 			try {
 				$json = json_decode($stdout);
 				elog($json);
 			} catch (Exception $e) {
+				elog('Failed to send transfer deploy for order #'.$order_id);
 				elog($e);
 			}
 
@@ -65,6 +95,18 @@ if($selection) {
 				SET fulfilled = 1,
 				sent_at = '$sent_at',
 				deploy_hash = '$deploy_hash'
+				WHERE id = $order_id
+			";
+
+			$db->do_query($query);
+
+			if(file_exists($secret_key_path)) {
+				unlink($secret_key_path);
+			}
+		} else {
+			$query = "
+				UPDATE orders
+				SET fulfilled = 2
 				WHERE id = $order_id
 			";
 
